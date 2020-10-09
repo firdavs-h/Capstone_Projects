742d5bd5ea72fdd5d933e15412804ebe3bec060d
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-10-09 09:40:08 -0400

    added main manu method to vending

--- src/test/java/com/techelevator/VendingMachineTests.java
+++ src/test/java/com/techelevator/VendingMachineTests.java
@@ -8,6 +8,17 @@ import java.util.List;
 import org.junit.Assert;
 
 public class VendingMachineTests {
+	
+	VendingMachine testVendingMachine =new VendingMachine();
 
 
+	@Test
+	public void mainManu_various_tests() {
+		//input 3 in Scanner should return 3
+		boolean testInputs=(testVendingMachine.mainManu().equals("1") || testVendingMachine.mainManu().equals("2") ||
+				testVendingMachine.mainManu().equals("3") || testVendingMachine.mainManu().equals("4"));
+		
+		Assert.assertEquals(true, testInputs);
+	}
+	
 }
