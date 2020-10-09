2ffa6782315a945aa887c64e49a3dac2e2240da7
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-10-09 15:24:52 -0400

    updated Sales report

--- src/main/java/com/techelevator/Sales.java
+++ src/main/java/com/techelevator/Sales.java
@@ -3,51 +3,57 @@ package com.techelevator;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TreeMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Sales extends Inventory {
+
+	private Map<String, Double> salesMap = new TreeMap<String, Double>();
 	
-	private Map<String, Double> salesMap = new HashMap<String, Double>();
-	private Inventory currentInvetory=new Inventory();
 
 	public Sales() {
 		setSales();
 	}
-	
+
 	public void setSales() {
-		Map<String, Product> temp=super.inventoryMap;
-		
+		Map<String, Product> temp = super.inventoryMap;
+
 		for (Product product : temp.values()) {
-			
+
 			salesMap.put(product.getProductName(), 0.0);
 		}
 	}
-	
+
 	public void printSales() {
-		File report = new File("Sales_report.txt");
-		if(report.exists()) {
-			report.delete();
-		}
-		try(FileWriter fileWrite =new FileWriter(report,true)){
-		for (String key: salesMap.keySet()) {
-			fileWrite.write(key+"|"+salesMap.get(key)+"\n");
-			System.out.println(key+"|"+salesMap.get(key));
-		}
-			
+
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("MM-dd-uuuu_hh:mm _a");
+
+		File report = new File("Sales_report" + formatTime.format(date) + ".txt");
+		double totalSales=0;
+		try (FileWriter fileWrite = new FileWriter(report, true)) {
+			for (String key : salesMap.keySet()) {
+				totalSales+=salesMap.get(key);
+				fileWrite.write(key + " | " + salesMap.get(key) + "\n");
+				System.out.println(key + " | " + salesMap.get(key));
+			}
+			System.out.println("\nTOTAL SALES: "+totalSales);
+
 		} catch (IOException e) {
 			System.out.println("Not able to write sales report file");
 		}
-		
+
 	}
+
 	public void recordSale(String location) {
-		
-		String name =super.getName(location);
-		Double value =salesMap.get(name)+super.getPrice(location);
-		
-		salesMap.put(name,value);
-		
+
+		String name = super.getName(location);
+		Double value = salesMap.get(name) + super.getPrice(location);
+
+		salesMap.put(name, value);
+
 	}
 
 	public Map<String, Double> getSalesMap() {
--- src/test/java/com/techelevator/SalesTest.java
+++ src/test/java/com/techelevator/SalesTest.java
@@ -1,6 +1,6 @@
 package com.techelevator;
 
-import org.junit.Assert;
+import org.junit.Assert; 
 import org.junit.Test;
 
 public class SalesTest {
@@ -10,19 +10,17 @@ public class SalesTest {
 	
 	@Test
 	public void setSales_Cloud_Popcorn_print_00() {
-		//testInventory.setInventory();
-		testSales.setSales();
-		
+
 		Assert.assertEquals(0.0, testSales.getSalesMap().get("Cloud Popcorn"),0);
 	}
 	@Test
 	public void printSalesTest() {
-		testSales.setSales();
+		
 		testSales.printSales();
 	}
 	@Test
 	public void recordSales_for_A1_updates_Potato_Crisps_to_3_05(){
-		testSales.setSales();
+		
 		testSales.recordSale("A1");
 		Assert.assertEquals(3.05, testSales.getSalesMap().get("Potato Crisps"),0);
 		testSales.printSales();
