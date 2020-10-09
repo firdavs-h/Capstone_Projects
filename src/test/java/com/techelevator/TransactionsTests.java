6b7d1ae43e0e570f9b27c8dfd425318bdacad0e0
Jesse Walls <drummerjw505@yahoo.com>
2020-10-09 16:36:48 -0400

    transactiontest

--- src/test/java/com/techelevator/TransactionsTests.java
+++ src/test/java/com/techelevator/TransactionsTests.java
@@ -8,11 +8,5 @@ public class TransactionsTests {
 		String actual = test.feedMoney(5);
 		Assert.assertEquals("5 dollars accepted", actual );
 	}
-	@Test
-	public void setBalance_test() {
-		Transactions test = new Transactions();
-		int balance = 0;
-		String actual = test.feedMoney(5);
-		Assert.assertEquals(balance+= 5, actual);
-	}
+
 }
