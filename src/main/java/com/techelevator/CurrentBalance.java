21245c317d2d6e3021fc980fc95b881d4b7f47f2
Sean Hunt <seanthunt@gmail.com>
2020-10-09 09:46:13 -0400

    Updated CurrentBalance to return double for purchase

--- src/main/java/com/techelevator/CurrentBalance.java
+++ src/main/java/com/techelevator/CurrentBalance.java
@@ -1,19 +1,24 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class CurrentBalance {
 
-	private double balance;
+	protected BigDecimal balance;
 	
 	public CurrentBalance() {
-		this.balance = 0;
+		this.balance = BigDecimal.ZERO;
 	}
 
 	public double getBalance() {
-		return balance;
+		double doubleBalance = balance.doubleValue();
+		return doubleBalance;
 	}
 
 	public void setBalance(double balance) {
-		this.balance += balance;
+		this.balance = this.balance.add(new BigDecimal(balance)).setScale(2, RoundingMode.HALF_EVEN);
 	}
 	
 	public String change() {
@@ -23,23 +28,32 @@ public class CurrentBalance {
 		int nickel = 0;
 		int penny = 0;
 		
-		while(this.balance >= 0.01) {
-			if(this.balance > 1) {
-				dollar++;
-				this.balance -= 1.0;
-			} else if(this.balance > 0.25) {
-				quarter++;
-				this.balance -= 0.25;
-			} else if(this.balance > 0.1) {
-				dime++;
-				this.balance -= 0.1;
-			} else if(this.balance > 0.05) {
-				nickel++;
-				this.balance -= 0.05;
-			} else if(this.balance >= 0.01) {
-				penny++;
-				this.balance -= 0.01;
+		BigDecimal qCoin = new BigDecimal(0.25).setScale(2, RoundingMode.HALF_EVEN);
+		BigDecimal dCoin = new BigDecimal(0.10).setScale(2, RoundingMode.HALF_EVEN);
+		BigDecimal nCoin = new BigDecimal(0.05).setScale(2, RoundingMode.HALF_EVEN);
+		BigDecimal pCoin = new BigDecimal(0.01).setScale(2, RoundingMode.HALF_EVEN);
+		try {
+			while(this.balance.compareTo(pCoin) == 1) {
+				if(this.balance.compareTo(BigDecimal.ONE) == 1) {
+					dollar++;
+					this.balance.subtract(BigDecimal.ONE);
+				} else if(this.balance.compareTo(qCoin) == 1) {
+					quarter++;
+					this.balance.subtract(qCoin);
+				} else if(this.balance.compareTo(dCoin) == 1) {
+					dime++;
+					this.balance.subtract(dCoin);
+				} else if(this.balance.compareTo(nCoin) == 1) {
+					nickel++;
+					this.balance.subtract(nCoin);
+				} else if(this.balance.compareTo(pCoin) == 1) {
+					penny++;
+					this.balance.subtract(pCoin);
+				}
 			}
+		}
+		catch(Exception e) {
+			System.out.println("infinite loop");
 		}
 		String changeDollars = (dollar == 0 ? "" : (dollar > 1 ? ((String)(dollar + " dollars")): ((String)(dollar + " dollar"))));
 		String changeQuarter = (quarter == 0 ? "" : (quarter > 1 ? ((String)(quarter + " quarters")): ((String)(quarter + " quarter"))));
@@ -47,7 +61,7 @@ public class CurrentBalance {
 		String changeNickel =(nickel == 0 ? "" : (nickel + " nickel"));
 		String changePenny = (penny == 0 ? "" : (penny > 1 ? ((String)(penny + " pennies")): ((String)(penny + " penny"))));
 		String changeDollarsCoins = (changeDollars + (dollar == 0 ? "" : ", ") + changeQuarter + (quarter == 0 ? "" : ", ") + changeDime + (dime == 0 ? "" : ", ") + changeNickel + (nickel == 0 ? "" : ", ") + changePenny);
-		this.balance = 0;
+		this.balance = BigDecimal.ZERO;
 		return changeDollarsCoins;
 	}
 	
