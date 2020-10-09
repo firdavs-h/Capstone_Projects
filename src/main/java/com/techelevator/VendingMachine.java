067350e19c2edfa91b8a088171c1537d9cd10d2c
Firdavs Hasanov <firdavs_h2@outlook.com>
2020-10-09 16:55:32 -0400

    added checker method for slot input

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,127 +1,117 @@
 package com.techelevator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.HashMap;
+import java.util.TreeSet;
 
 public class VendingMachine {
-	//Map<String, Inventory> itemList = new HashMap<>();
-	Transactions log =new Transactions();
-	Scanner myScan=new Scanner(System.in);
-    private Sales inventory=new Sales();
-    Transactions balance = new Transactions();
+
+	Transactions log = new Transactions();
+	Scanner myScan = new Scanner(System.in);
+	private Sales inventory = new Sales();
+	Transactions balance = new Transactions();
+
 	public VendingMachine() {
-		
+
 	}
 
-
-	
-		
-	
 	public String purchase() throws IOException {
 		double preBalance = balance.getBalance();
 		System.out.println("Please enter slot#: ");
 		String location = myScan.nextLine().toUpperCase();
-		
-		
-		
-		if(inventory.getQuantity(location) == 0) {
+
+		if (inventory.getQuantity(location) == 0) {
 			return "There are no more left.";
 		}
-		if(inventory.getPrice(location) <= balance.getBalance()) {
+		if (inventory.getPrice(location) <= balance.getBalance()) {
 			inventory.getProduct(location).dispense();
 			balance.payment(inventory.getPrice(location));
-			log.printTransaction(inventory.getName(location), location, balance.displayBalance(), preBalance);  
+			log.printTransaction(inventory.getName(location), location, balance.displayBalance(), preBalance);
 			inventory.recordSale(location);
-				return "You have purchased 1 " + inventory.getName(location) + flavorText(location);
-				
+			return "You have purchased 1 " + inventory.getName(location) + flavorText(location);
+
 		} else {
 			return "The cost of this item exceeds your current balance";
 		}
-		
-		
-		
+
 	}
-	
-	
-
-
-
-
 
 	public String mainMenu() {
 		String input;
-		System.out.println("(1) Display Vending Machine Items\n" + 
-				"(2) Purchase\n" + 
-				"(3) Exit");
-		input=myScan.nextLine();
-		boolean validInput=(input.equals("1")||input.equals("2")||input.equals("3")||input.equals("4"));
-		while(!validInput) {
-		
+		System.out.println("(1) Display Vending Machine Items\n" + "(2) Purchase\n" + "(3) Exit");
+		input = myScan.nextLine();
+		boolean validInput = (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4"));
+		while (!validInput) {
+
 			System.out.println("Please select 1, 2 or 3");
-			input=myScan.nextLine();
-			validInput=(input.equals("1")||input.equals("2")||input.equals("3")||input.equals("4"));
+			input = myScan.nextLine();
+			validInput = (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4"));
 		}
 		return input;
 	}
+
 	public String purchaseMenu() {
 		String input;
-		System.out.println("(1) Feed Money\n" + 
-				"(2) Select Product\n" + 
-				"(3) Finish Transaction \n"
-				+ "\tCurrent money provided: " + balance.displayBalance()); 
-		input=myScan.nextLine();
-		boolean validInput=(input.equals("1")||input.equals("2")||input.equals("3"));
-		while(!validInput) {
-		
+		System.out.println("(1) Feed Money\n" + "(2) Select Product\n" + "(3) Finish Transaction \n"
+				+ "\tCurrent money provided: " + balance.displayBalance());
+		input = myScan.nextLine();
+		boolean validInput = (input.equals("1") || input.equals("2") || input.equals("3"));
+		while (!validInput) {
+
 			System.out.println("Please select 1, 2 or 3");
-			input=myScan.nextLine();
-			validInput=(input.equals("1")||input.equals("2")||input.equals("3"));
+			input = myScan.nextLine();
+			validInput = (input.equals("1") || input.equals("2") || input.equals("3"));
 		}
 		return input;
 	}
+
 	public void displayMenu() {
 		inventory.displayInventory();
-		
+
 	}
-	
+
 	public void feedMoneyExt() throws IOException {
 		double preBalance = balance.getBalance();
 		System.out.println("Enter bills: ");
 		String input = myScan.nextLine();
 		int output = Integer.parseInt(input);
 		balance.feedMoney(output);
-		log.printTransaction("FEED", " MONEY: ", balance.displayBalance() , preBalance); 
-		
+		log.printTransaction("FEED", " MONEY: ", balance.displayBalance(), preBalance);
+
 	}
-	
+
 	public void changeMachine() throws IOException {
 		double preBalance = balance.getBalance();
-		System.out.println("Your change is: " + balance.change() + "\n Your current balance is: " + balance.displayBalance());
+		System.out.println(
+				"Your change is: " + balance.change() + "\n Your current balance is: " + balance.displayBalance());
 		log.printTransaction("GIVE", " CHANGE: ", balance.displayBalance(), preBalance);
 	}
-	
+
 	public void reportDatSales() {
 		inventory.printSales();
 	}
+
 	public String flavorText(String location) {
-		if(inventory.getType(location).equals("Chip")) {
+		if (inventory.getType(location).equals("Chip")) {
 			return ". Crunch Crunch, Yum!";
-		} else if(inventory.getType(location).equals("Candy")) {
+		} else if (inventory.getType(location).equals("Candy")) {
 			return ". Munch Munch, Yum!";
-		} else if(inventory.getType(location).equals("Drink")) {
+		} else if (inventory.getType(location).equals("Drink")) {
 			return ". Glug Glug, Yum!";
 		} else if (inventory.getType(location).equals("Gum")) {
 			return ". Chew Chew, Yum!";
 		}
 		return null;
+	}
+
+	public boolean slotCheck(String location) {
+		Set<String> slots = new TreeSet<String>(inventory.getInventoryMap().keySet());
+		boolean slotExist = true;
+		if (!(slots.contains(location))) {
+			System.out.println("Invalid slot number");
+			slotExist = false;
+		}
+		return slotExist;
 	}
 }
--- src/test/java/com/techelevator/VendingMachineTests.java
+++ src/test/java/com/techelevator/VendingMachineTests.java
@@ -8,25 +8,30 @@ import java.util.List;
 import org.junit.Assert;
 
 public class VendingMachineTests {
-	
-	VendingMachine testVendingMachine =new VendingMachine();
 
+	VendingMachine testVendingMachine = new VendingMachine();
 
 	@Test
 	public void mainMenu_various_tests() {
-		//input 1,2,3 or 4 in Scanner should return 1,2,3 or 4 
-		boolean testInputs=(testVendingMachine.mainMenu().equals("1") || testVendingMachine.mainMenu().equals("2") ||
-				testVendingMachine.mainMenu().equals("3") || testVendingMachine.mainMenu().equals("4"));
-		
+		// input 1,2,3 or 4 in Scanner should return 1,2,3 or 4
+		boolean testInputs = (testVendingMachine.mainMenu().equals("1") || testVendingMachine.mainMenu().equals("2")
+				|| testVendingMachine.mainMenu().equals("3") || testVendingMachine.mainMenu().equals("4"));
+
 		Assert.assertEquals(true, testInputs);
 	}
+
 	@Test
 	public void purchaseMenu_various_tests() {
-		//input 1,2 or 3in Scanner should return 1,2 or 3 
-		boolean testInputs=(testVendingMachine.purchaseMenu().equals("1") || testVendingMachine.purchaseMenu().equals("2") ||
-				testVendingMachine.purchaseMenu().equals("3"));
-		
+		// input 1,2 or 3in Scanner should return 1,2 or 3
+		boolean testInputs = (testVendingMachine.purchaseMenu().equals("1")
+				|| testVendingMachine.purchaseMenu().equals("2") || testVendingMachine.purchaseMenu().equals("3"));
+
 		Assert.assertEquals(true, testInputs);
 	}
-	
+
+	@Test
+	public void slotChecker_G1_return_false() {
+
+		Assert.assertEquals(false, testVendingMachine.slotCheck("G1"));
+	}
 }
